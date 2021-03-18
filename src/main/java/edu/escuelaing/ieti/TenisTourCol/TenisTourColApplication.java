99500819f2eecec5afd0da1b475b4cd25b55cdcb
David Andres Herrera <damoya2000@gmail.com>
2021-03-18 17:40:24 -0500

    Base 1

---
+++ src/main/java/edu/escuelaing/ieti/TenisTourCol/TenisTourColApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.ieti.TenisTourCol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenisTourColApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenisTourColApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/escuelaing/ieti/TenisTourCol/TenisTourColApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.ieti.TenisTourCol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenisTourColApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
