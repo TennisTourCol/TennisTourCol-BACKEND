7378ef10d253f3084a31fd97ae863a2c51ad3ec2
JuanManuelHerreraMoya <57697133+JuanManuelHerreraMoya@users.noreply.github.com>
2021-04-02 18:42:08 -0500

    #5 Ver Perfil Player Correccion

--- src/test/java/edu/escuelaing/ieti/tenistourcol/TenisTourColApplicationTests.java
+++ src/test/java/edu/escuelaing/ieti/tenistourcol/TenisTourColApplicationTests.java
@@ -20,6 +20,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import edu.escuelaing.ieti.tenistourcol.model.ExceptionResponse;
+import edu.escuelaing.ieti.tenistourcol.model.Response;
+import edu.escuelaing.ieti.tenistourcol.model.SuccessResponse;
+import edu.escuelaing.ieti.tenistourcol.model.Tournament;
 
 import java.lang.reflect.Type;
 import java.math.BigInteger;
