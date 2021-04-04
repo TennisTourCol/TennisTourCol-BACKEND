a9183fd2aefb0dc1482c5bfc43b74b8a1c023276
David Andres Herrera <damoya2000@gmail.com>
2021-04-04 12:50:30 -0500

    #14 inscribir torneo

--- src/main/java/edu/escuelaing/ieti/tenistourcol/service/PlayerService.java
+++ src/main/java/edu/escuelaing/ieti/tenistourcol/service/PlayerService.java
@@ -1,7 +1,11 @@
 package edu.escuelaing.ieti.tenistourcol.service;
 
+import edu.escuelaing.ieti.tenistourcol.model.Player;
 import edu.escuelaing.ieti.tenistourcol.model.Response;
 
 public interface PlayerService {
     Response getUserById(String id);
+    Response addTournament(String idUser, String idTournament);
+    Response getSchedule(String id);
+    Response createPlayer(Player player);
 }
