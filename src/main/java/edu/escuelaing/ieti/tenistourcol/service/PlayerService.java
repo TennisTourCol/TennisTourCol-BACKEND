2c5ada518df4088ea269bd3cb8112bb6e6c9e823
DavidAndresHerrera <53908702+DavidAndresHerrera@users.noreply.github.com>
2021-04-04 12:10:01 -0500

    Revert "#14 inscripcion a torneo"

--- src/main/java/edu/escuelaing/ieti/tenistourcol/service/PlayerService.java
+++ src/main/java/edu/escuelaing/ieti/tenistourcol/service/PlayerService.java
@@ -1,11 +1,7 @@
 package edu.escuelaing.ieti.tenistourcol.service;
 
-import edu.escuelaing.ieti.tenistourcol.model.Player;
 import edu.escuelaing.ieti.tenistourcol.model.Response;
 
 public interface PlayerService {
     Response getUserById(String id);
-    Response addTournament(String idUser, String idTournament);
-    Response getSchedule(String id);
-    Response createPlayer(Player player);
 }
